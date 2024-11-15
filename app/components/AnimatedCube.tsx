79b7c75fc256c152cca7fa5dced99f13316d2306
Moizes Sousa <ry.diffusion@proton.me>
2024-11-15 16:38:18 +0000

    Make loading more fun

--- app/components/AnimatedCube.tsx
+++ app/components/AnimatedCube.tsx
@@ -1,9 +1,18 @@
 import CSS from "@/app/styles/Cube.module.css"
+import SuaveCss from "@/app/styles/Suave.module.css"
 
 export default function AnimatedCube() {
-    return <div className={CSS.cube}>
-        <div className={CSS.side}></div>
-        <div className={CSS.side}></div>
+    return <div className={`${CSS.cube} border-solid border-green-200 border-2 rounded-full`}>
+        <div className={CSS.side}>
+            <p className={`${SuaveCss.gradient} mt-auto ml-auto text-center text-sm`}>
+                S
+            </p>
+        </div>
+        <div className={CSS.side}>
+            <p className={`${SuaveCss.gradient} mt-auto ml-auto text-center text-sm rotate-90`}>
+                S
+            </p>
+        </div>
         <div className={CSS.side}></div>
         <div className={CSS.side}></div>
         <div className={CSS.side}></div>
