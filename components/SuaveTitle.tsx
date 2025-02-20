c0f614aa1ec07dd88598fb535e97a62919de9d4a
Ryster <ry.diffusion@proton.me>
2025-02-19 21:33:28 -0300

    Rebrand logo

--- components/SuaveTitle.tsx
+++ components/SuaveTitle.tsx
@@ -2,10 +2,10 @@ import SuaveCss from '@/app/styles/Suave.module.css'
 import Link from 'next/link'
 
 export default function SuaveTitle() {
-    return <Link className={`flex flex-col center`} href={'/public'}>
-        <h1 className={`${SuaveCss.gradient} px-12 text-8xl`}>
-            Suave
-        </h1>
+    return <Link className={`flex flex-col center`} href={'/'}>
+        <svg height="100" stroke="#D2A8FF" strokeWidth="2" className={SuaveCss.suave} width="100%">
+            <text x="50%" dominantBaseline="middle" textAnchor="middle" y="50%">Suave</text>
+        </svg>
 
         <p className="text-sm self-end"> Por Moizes J. Sousa </p>
     </Link>
