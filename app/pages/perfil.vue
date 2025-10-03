21eb393a8aeb141604fa27c60563ecced115262f
Moizes Sousa <ry.diffusion@proton.me>
2025-10-03 12:06:44 +0000

    update

--- app/pages/perfil.vue
+++ app/pages/perfil.vue
@@ -290,6 +290,16 @@ const { userData, pending, error, refresh } = useMyData();
 // analytics proxy (similar to login.vue)
 const { proxy: rybbit } = useScriptRybbitAnalytics();
 
+onMounted(() => {
+  const correctUserId = userData?.value?.matricula;
+  const rybbitUserId = rybbit.getUserId();
+
+  if (correctUserId != rybbitUserId) {
+    rybbit.identity(correctUserId);
+  }
+  
+});
+
 const isDarkMode = computed({
   get: () => themeStore.colorMode === "dark",
   set: (value) => {
